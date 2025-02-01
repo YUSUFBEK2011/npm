3583f769810be424509c2a9d87693492a6856daf
YUSUFBEK2011 <Mashrabovnaxxx@gmail.com>
2025-02-01 13:56:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const typed = new Typed('#text', {
+    strings: ['<i>First</i> sentence.', 'Text Gradients.'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true
+  });
+const typed2 = new Typed('#text2', {
+    strings: ['Makes Your Hedlines Stand Out.'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true
+  });
